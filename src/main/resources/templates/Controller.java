ec553d033e54bfbfab02566cddabea1b89e11ce4
dhiego62 <dhiegofer62@gmail.com>
2025-04-06 20:21:02 -0300

    demo test

---
+++ src/main/resources/templates/Controller.java
@@ -0,0 +1,23 @@
+package com.example.securing_web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
